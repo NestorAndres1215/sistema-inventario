620e1109c5d67ff4c00ef52f31a99f86d0405286
NestorAndres1215 <atiroandres@gmail.com>
2026-02-10 16:44:13 -0500

    subiendo

--- FRONT-INVENTARIO/src/app/pages/admin/productos/listar-producto-desactivadas/listar-producto-desactivadas.component.ts
+++ FRONT-INVENTARIO/src/app/pages/admin/productos/listar-producto-desactivadas/listar-producto-desactivadas.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { Router } from '@angular/router';
+import { MENSAJES, TITULO_MESAJES } from 'src/app/core/constants/messages';
+import { AlertService } from 'src/app/core/services/alert.service';
 import { ProductoService } from 'src/app/core/services/producto.service';
 import Swal from 'sweetalert2';
 
@@ -18,14 +20,25 @@ export class ListarProductoDesactivadasComponent implements OnInit {
   proveedorId: string = '';
   productos: any[] = [];
 
-  constructor(private http: HttpClient,
+  constructor(private alertService: AlertService,
     private productoService: ProductoService,
     private router: Router) { }
+
+    botonesConfig = {
+  ver: true,
+  activar: true
+};
   ngOnInit(): void {
     this.obtenerProducto();
   }
+
+  verProducto(producto: any) {
+  this.router.navigate(['/admin/producto/detalle', producto.productoId]);
+}
+
+
   buscarPorNombre() {
-    try {
+
       if (this.nombre && this.productos) {
         this.productos = this.productos.filter((proveedor: any) =>
           proveedor.nombre.toLowerCase().includes(this.nombre.toLowerCase())
@@ -34,22 +47,24 @@ export class ListarProductoDesactivadasComponent implements OnInit {
         this.restaurarProveedores();
         console.log("Ingrese un nombre o RUC para buscar.");
       }
-    } catch (error) {
-      console.log("Error en la búsqueda: ", error);
-      // Realizar acciones de manejo de errores, como mostrar un mensaje al usuario
-    }
+
   }
+  columnas = [
+    { clave: 'productoId', etiqueta: 'Código' },
+    { clave: 'nombre', etiqueta: 'Nombre' },
+    { clave: 'descripcion', etiqueta: 'Descripción' },
+    { clave: 'precio', etiqueta: 'Precio' },
+    { clave: 'stock', etiqueta: 'Stock' },
+    { clave: 'proveedor.nombre', etiqueta: 'Proveedor' }
+  ];
 
   restaurarProveedores() {
-    this.nombre = ''; // Restablecer el valor del nombre a vacío
+    this.nombre = '';
 
     this.productoService.listarProductosDesactivados().subscribe(
       (proveedores: any) => {
         this.productos = proveedores;
       },
-      (error: any) => {
-        console.log("Error al obtener las categorías: ", error);
-      }
     );
   }
 
@@ -58,17 +73,14 @@ export class ListarProductoDesactivadasComponent implements OnInit {
       (productos: any) => {
         this.productos = productos;
       },
-      (error: any) => {
-        console.log("Error al obtener las productos: ", error);
-      }
     );
   }
 
 
 
 
-  pageSize = 3; // Tamaño de página (número de elementos por página)
-  pageIndex = 0; // 
+  pageSize = 3;
+  pageIndex = 0;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   onPageChange(event: PageEvent) {
     this.pageIndex = event.pageIndex;
@@ -77,24 +89,11 @@ export class ListarProductoDesactivadasComponent implements OnInit {
   activarProducto(productoId: any): void {
     this.productoService.activarProducto(productoId).subscribe(
       (respuesta: any) => {
-        // Desactivación exitosa
-        Swal.fire({
-          icon: 'success',
-          title: 'Producto desactivado',
-          text: respuesta.mensaje
-        });
+        this.alertService.aceptacion(TITULO_MESAJES.ACTIVADO, MENSAJES.ACTIVADO);
 
-        // Actualizar la lista de categorías activadas
         this.obtenerProducto();
       },
-      (error: any) => {
-        // Error al desactivar la categoría
-        Swal.fire({
-          icon: 'error',
-          title: 'Error al desactivar la producto',
-          text: error.error.mensaje
-        });
-      }
+
     );
   }
 }
